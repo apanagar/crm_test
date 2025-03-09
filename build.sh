b8743f1f567ea119a4f005a432c8bc562d47638b
ajay <apanagar@gmail.com>
2025-03-08 23:02:50 -0800

    qlite

--- build.sh
+++ build.sh
@@ -5,47 +5,20 @@ set -o errexit
 # Install dependencies
 pip install -r requirements.txt
 
-# Wait for database to be ready
-python << END
-import sys
-import time
-import dj_database_url
-import psycopg2
-from urllib.parse import urlparse
-
-db_config = dj_database_url.config()
-if not db_config:
-    print("No DATABASE_URL found, using SQLite")
-    sys.exit(0)
-
-db_url = urlparse(db_config['NAME'])
-while True:
-    try:
-        conn = psycopg2.connect(
-            dbname=db_url.path[1:],
-            user=db_config.get('USER'),
-            password=db_config.get('PASSWORD'),
-            host=db_url.hostname
-        )
-        conn.close()
-        break
-    except psycopg2.OperationalError:
-        sys.stdout.write(".")
-        sys.stdout.flush()
-        time.sleep(1)
-print("\nDatabase is ready!")
-END
-
 # Make migrations
+echo "Creating migrations..."
 python manage.py makemigrations
 
 # Run migrations
+echo "Applying migrations..."
 python manage.py migrate --noinput
 
 # Collect static files
+echo "Collecting static files..."
 python manage.py collectstatic --no-input
 
 # Create superuser
+echo "Creating superuser..."
 DJANGO_SUPERUSER_EMAIL=${DJANGO_SUPERUSER_EMAIL:-"admin@example.com"}
 DJANGO_SUPERUSER_USERNAME=${DJANGO_SUPERUSER_USERNAME:-"admin"}
 DJANGO_SUPERUSER_PASSWORD=${DJANGO_SUPERUSER_PASSWORD:-"Admin123!@#"}
--- salesforce_clone/settings.py
+++ salesforce_clone/settings.py
@@ -87,11 +87,14 @@ WSGI_APPLICATION = 'salesforce_clone.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:////' + str(BASE_DIR / 'db.sqlite3'),
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+if DEBUG:
+    print(f"Database configuration: {DATABASES['default']}")
 
 
 # Password validation
