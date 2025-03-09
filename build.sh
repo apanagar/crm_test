6fa509cb742f9dd78af69845e7d637a42c31b26a
ajay <apanagar@gmail.com>
2025-03-08 21:51:38 -0800

    superuser

--- build.sh
+++ build.sh
@@ -9,4 +9,11 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 # Run migrations
-python manage.py migrate 
+python manage.py migrate
+
+# Create superuser
+DJANGO_SUPERUSER_EMAIL=${DJANGO_SUPERUSER_EMAIL:-"admin@example.com"}
+DJANGO_SUPERUSER_USERNAME=${DJANGO_SUPERUSER_USERNAME:-"admin"}
+DJANGO_SUPERUSER_PASSWORD=${DJANGO_SUPERUSER_PASSWORD:-"admin123!"}
+
+python manage.py createsuperuser --noinput 
